9c4158ac49f0f1883bdd8b5985360479d1e103df
Andy Bolos <andybolos@MacBook-Pro-2.local>
2021-06-01 21:06:39 -0600

    lecture work-in-progress

--- src/api/fetchMissions.js
+++ src/api/fetchMissions.js
@@ -4,7 +4,7 @@ export const fetchMissions = () => {
   return axios
     .get("https://api.spacexdata.com/v3/missions")
     .then(res => {
-      // console.log(res);
+      console.log(res);
       return res;
     })
     .catch(err => {
