b397fc9e31021fe11e4863f6bc0e51b5aecd7660
Andy Bolos <andybolos@MacBook-Pro-2.local>
2021-06-01 20:21:08 -0600

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import MissionForm from './MissionForm'
 
 test('MissionForm renders correctly without errors', () => {
