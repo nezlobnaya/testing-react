a916f8196b1d9c7bc9b213675f78c12198a011b7
Andy Bolos <andybolos@MacBook-Pro-2.local>
2021-06-01 21:15:11 -0600

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import MissionsList from './MissionsList'
 
+const missions = [
+    { mission_name: 'Mission 1', mission_id: 'mission_id_1'},
+    { mission_name: 'Mission 2', mission_id: 'mission_id_2'}
+]
+
 test('renders without errors', () => {
     render(<MissionsList missions={[]} />)
 })
