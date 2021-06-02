e1bafc85f7a58180484e7393b4dd92db008e0422
Andy Bolos <andybolos@MacBook-Pro-2.local>
2021-06-01 21:12:06 -0600

    lecture work-in-progress

---
+++ src/components/MissionsList.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import MissionsList from './MissionsList'
+
+test('renders without errors', () => {
+    render(<MissionsList />)
+})
